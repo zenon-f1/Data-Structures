c9a041ae9800685733b2e6a5c507feed7c97ab96
Helix <starc.vinay44@gmail.com>
2024-02-09 02:16:09 +0530

    Maths Functions

---
+++ C++/Maths/math.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // Maths Functions
+    cout << max(45, 44) << endl;
+    cout << max(56, max(8989, 45435)) << endl;
+    cout << sqrt(16) << endl;
+    cout << cbrt(27) << endl;
+    cout << pow(5, 5) << endl;
+    cout << sin(0) << endl;
+    cout << sinh(45) << endl;
+    cout<< tan(45) << endl;
+    cout<< tanh(90) << endl;
+    cout << cos(45) << endl;
+    cout << cosh(45) << endl;
+    cout << round(45.67) << endl;
+    cout << log(10) << endl;
+    cout << abs(-45.788) << endl;
+    cout << ceil(34.22) << endl;  // up nearest integer
+    cout << floor(34.22) << endl; // down nearest integer
+    cout << exp(100) << endl;
+    cout << fabs(-45.667) << endl;
+    cout << fmax(100.777, 78.999) << endl; // highest value of a floating x and y
+    cout << fmin(100.777, 78.999) << endl; // lowest value of a floating x and y
+    cout << fmod(10.98, 3.2) << endl;      // floating point remainder
+
+    return 0;
+}
